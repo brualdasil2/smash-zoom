82f9478fc388f412479797ace4e685c324034c81
Bruno Silveira <dasilveira.bru@terra.com.br>
2022-05-04 15:12:12 -0300

    Alts aleatórios

--- src/App.js
+++ src/App.js
@@ -7,6 +7,7 @@ function App() {
   const [zooming, setZooming] = useState(false)
   const [binCounter, setBinCounter] = useState(true)
   const [zoomOffset, setZoomOffset] = useState({x: 0, y: 0})
+  const [alt, setAlt] = useState("")
 
   function randomizeOffset() {
     const xOffset = Math.round(-10 + Math.random()*20);
@@ -16,6 +17,9 @@ function App() {
 
   function randomizeFighter() {
     const randomNumber = Math.round(Math.random()*82)
+    const randomAlt = Math.round(1 + Math.random()*7)
+    const altString = randomAlt === 1 ? "" : `${randomAlt}`
+    setAlt(altString)
     setFighterNumber(randomNumber)
     setBinCounter(!binCounter)
     randomizeOffset()
@@ -30,7 +34,7 @@ function App() {
   return (
     <div>
       <ImgContainer>
-        <ZoomImage zoomOffset={zoomOffset} zooming={zooming} zoomAnimation={binCounter ? "zoom" : "zoom2"} src={`https://www.smashbros.com/assets_v2/img/fighter/${fighters[fighterNumber]}/main.png`}/>
+        <ZoomImage zoomOffset={zoomOffset} zooming={zooming} zoomAnimation={binCounter ? "zoom" : "zoom2"} src={`https://www.smashbros.com/assets_v2/img/fighter/${fighters[fighterNumber]}/main${alt}.png`}/>
       </ImgContainer>
       <Button onClick={randomizeFighter}>Sortear</Button>
       {zooming ? (
